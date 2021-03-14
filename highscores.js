2c209d488681ed462fe851a2cc39d2c32f5ab893
Fasica Robi <fasicasr@gmail.com>
2021-03-13 20:04:26 -0500

    updated clearing function

--- highscores.js
+++ highscores.js
@@ -3,20 +3,30 @@ let clear = document.querySelector("#clear");
 let goBack = document.querySelector("#goBack");
 
 //Retrieve local storage
-let highScores = localStorage.getItem ('highScores')
+let highScores = localStorage.getItem ('highScores') || []
 highScores = JSON.parse(highScores)
 
-listHighscores.value =  JSON.stringify(highScores)
+if (highScores) {
+for(let i = 0; i < highScores.length; i++) {
+    let score = document.querySelector('#score' + (i + 1))
+    score.innerText = highScores[i].name+highScores[i].score
+    // alert(highScores[i].name)
+}
+}
+clear.addEventListener("click", function () {
+    localStorage.setItem('highScores', null)
+    window.location.replace('highScores.html')
+});
 
 // clear.addEventListener('click', highScores) {
 //     localStorage.clear();
 //     // location.reload();
 // };
 
-localStorage.setItem = finalScores, null
+// localStorage.setItem = finalScores, null
 
 
 //event listener to move to homepage
-goBack.addEventListener("Click", function () {
+goBack.addEventListener("click", function () {
     window.location.replace("index.html");
 });
